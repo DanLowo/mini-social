8c3e92ebb45c8795a81737b190b2e509433ea75e
DanLowo <danlowo@gmail.com>
2021-10-14 18:47:29 +0100

    auth with social media

---
+++ src/components/SocialAuth.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SocialAuth = () => {
+    return (
+            <span className="material-icons" style={{marginRight: 20}}>face</span>
+    )
+}
+
+export default SocialAuth
