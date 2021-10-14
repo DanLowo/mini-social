16e31ebe8dc9125c86d46e0ac19de284f2a2a4e6
DanLowo <danlowo@gmail.com>
2021-10-14 17:31:54 +0100

    start project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'material-design-icons/iconfont/material-icons.css'
+
+function App() {
+
+  return (
+    <div className="App">
+	<h1>Hello Dan!</h1>
+    </div>
+  );
+}
+
+export default App;
