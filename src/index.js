24b761fe6061ce34414f11dd861fdb27346a65d6
DanLowo <danlowo@gmail.com>
2021-10-15 14:55:47 +0100

    use react router dom

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store/'
 import ReactDOM from 'react-dom';
@@ -8,7 +9,9 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
