38a0c4554dceb264928f05c410724c488c7694a9
DanLowo <danlowo@gmail.com>
2021-10-15 14:51:30 +0100

    register page

--- src/Auth/Register.js
+++ src/Auth/Register.js
@@ -1,14 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 import '../assets/Auth.css'
 import InputField from '../components/InputField'
 import SocialAuth from '../components/SocialAuth'
+import { register, resetState } from '../store/user'
 
 const Register = () => {
+    const dispatch = useDispatch()
+    const { register: { success, error, msg } } = useSelector(state => state.user)
+
+    const router = useHistory()
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
-    const handleSubmit = (e) => {
+    useEffect(() => {
+        if(success) {
+            dispatch(resetState('register'))
+            router.push("/")
+        }
+    }, [success, router, dispatch])
+
+    const handleSubmit = async(e) => {
         e.preventDefault()
+        const details = {email, password}
+        await dispatch(register(details))
+        if(success) {
+            router.push("/")
+        }
+        
     }
 
     return (
@@ -19,14 +39,15 @@ const Register = () => {
                 <p className="sub">Master web development by making real-life projects. There are many paths for you to choose</p>
 
                 <form onSubmit={handleSubmit}>
-                    <InputField type="email" placeholder="Email Address" setText={setEmail} /> {email}
-                    <InputField type="password" placeholder="Password" setText={setPassword} /> {password}
+                    <InputField type="email" placeholder="Email Address" setText={setEmail} required />
+                    <InputField type="password" placeholder="Password" setText={setPassword} required />
+                    {error && <p className="error">{msg}</p>}
                     <button type="submit" className="submit">Start coding now</button>
                 </form>
                 <section className="loginWithSocial">
                     <sub className="continueWithSocial">or continue with these social media</sub>
-                    <SocialAuth /><SocialAuth /><SocialAuth />
-                    <sub>Already a member? <span style={{color: 'green'}}>Login</span> </sub>
+                    <SocialAuth type="register" />
+                    <sub>Already a member? <span style={{color: 'green'}} onClick={() => router.push("/login")}>Login</span> </sub>
                 </section>
             </div>
         </div>
