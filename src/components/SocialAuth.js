9ea5179151b8169539dc618a559dbba2eac87676
DanLowo <danlowo@gmail.com>
2021-10-15 14:55:03 +0100

    google auth

--- src/components/SocialAuth.js
+++ src/components/SocialAuth.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import { GoogleLogin } from 'react-google-login';
+import { useDispatch } from 'react-redux'
+import { login, register } from '../store/user';
 
-const SocialAuth = () => {
+const SocialAuth = ({ type }) => {
+
+  const dispatch = useDispatch()
+
+    const responseGoogle = async (response) => {
+      try {
+        const { profileObj: {email, name, imageUrl: image, googleId} } = response
+        if(type==='login') {   
+          const details = {email, googleId, type: 'google'}
+          await dispatch(login(details))
+        } else {
+          const details = {email, name, googleId, type: 'google', image}
+          await dispatch(register(details))
+        }
+      } catch (err) {
+        
+      }
+    }
+
     return (
-            <span className="material-icons" style={{marginRight: 20}}>face</span>
+        <GoogleLogin
+        clientId="542238092713-e1sgj23i345t255a71j0ps5je4rn58fl.apps.googleusercontent.com"
+        // render={renderProps => (
+        //   <button onClick={renderProps.onClick} disabled={renderProps.disabled}>This is my custom Google button</button>
+        // )}
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={() => console.log('Google Auth Failed')}
+        cookiePolicy={'single_host_origin'}
+      />
     )
 }
 
