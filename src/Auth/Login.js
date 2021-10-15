d911905303c3a7eb16304c4956404e481e6f1731
DanLowo <danlowo@gmail.com>
2021-10-15 14:51:21 +0100

    login page

--- src/Auth/Login.js
+++ src/Auth/Login.js
@@ -1,14 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 import '../assets/Auth.css'
 import InputField from '../components/InputField'
 import SocialAuth from '../components/SocialAuth'
+import { login, resetState } from '../store/user'
 
 const Login = () => {
+    const dispatch = useDispatch()
+    const { login: { success, error, msg } } = useSelector(state => state.user)
+    const router = useHistory()
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
-    const handleSubmit = (e) => {
+    useEffect(() => {
+        if(success) {
+            dispatch(resetState('login'))
+            router.push("/")
+        }
+    }, [success, router, dispatch])
+
+    const handleSubmit = async(e) => {
         e.preventDefault()
+        const details = {email, password}
+        await dispatch(login(details))
+        if(success) {
+            router.push("/")
+        }
+        
     }
 
     return (
@@ -18,14 +37,15 @@ const Login = () => {
                 <h4>Login</h4>
                 <br />
                 <form onSubmit={handleSubmit}>
-                    <InputField type="email" placeholder="Email Address" setText={setEmail} /> {email}
-                    <InputField type="password" placeholder="Password" setText={setPassword} /> {password}
+                    <InputField type="email" placeholder="Email Address" setText={setEmail} required />
+                    <InputField type="password" placeholder="Password" setText={setPassword} required />
+                    {error && <p className="error">{msg}</p>}
                     <button type="submit" className="submit">Login</button>
                 </form>
                 <section className="loginWithSocial">
                     <sub className="continueWithSocial">or continue with these social media</sub>
-                    <SocialAuth /><SocialAuth /><SocialAuth />
-                    <sub>Dont have an account yet? <span style={{color: 'green'}}>Register</span> </sub>
+                    <SocialAuth type="login" />
+                    <sub>Dont have an account yet? <span style={{color: 'green'}} onClick={() => router.push("/register")}>Register</span> </sub>
                 </section>
             </div>
         </div>
